8f90c12aa77308729bb69c260e2c24c05e3d2f44
Vladimir Boychev <vladboychev@gmail.com>
2023-10-02 01:12:29 +0300

    initial commit

---
+++ finlin/src/main/java/jbv/finlin/FinlinApplication.java
@@ -0,0 +1,13 @@
+package jbv.finlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinlinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinlinApplication.class, args);
+	}
+
+}
---
+++ finlin/src/test/java/jbv/finlin/FinlinApplicationTests.java
@@ -0,0 +1,13 @@
+package jbv.finlin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinlinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
